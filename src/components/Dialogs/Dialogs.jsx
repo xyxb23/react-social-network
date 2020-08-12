6a6dc6995d4b3b594f597d12327afd538434c491
xyxb23 <2226027+xyxb23@users.noreply.github.com>
2020-08-12 22:56:43 +0700

    Textarea ref, onclick example

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -7,6 +7,12 @@ const Dialogs = (props) => {
   let dialogs = props.state.dialogsData.map(dialog => <DialogItem key={dialog.id} state={dialog}/>);
   let messages = props.state.messagesData.map(message => <Message key={message.id} state={message}/>);
 
+  let newMessageElement = React.createRef();
+
+  let addMessage = () => {
+    alert(newMessageElement.current.value);
+  }
+
   return (
     <div className={classes.dialogs}>
       <div className={classes.dialogItems}>
@@ -14,6 +20,10 @@ const Dialogs = (props) => {
       </div>
       <div className={classes.messages}>
         {messages}
+        <div id={classes.addMessage}>
+          <textarea ref={newMessageElement}/>
+          <button onClick={addMessage}>Add message</button>
+        </div>
       </div>
     </div>);
 }
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,12 +5,17 @@ import Post from "./Post/Post"
 const MyPosts = (props) => {
   let posts = props.posts.map(post => <Post message={post.message} likes={post.likesCount}/>);
 
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+    alert(newPostElement.current.value);
+  }
+
   return (
     <div>
       <div className={classes.addPost}>
-        <textarea/>
-        <button>Add post</button>
-        <button>Remove post</button>
+        <textarea ref={newPostElement}/>
+        <button onClick={addPost}>Add post</button>
       </div>
       <div className={classes.posts}>
         {posts}
