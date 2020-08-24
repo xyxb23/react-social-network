a3359406ad26c0792da5f962db3334a316da6f80
xyxb23 <2226027+xyxb23@users.noreply.github.com>
2020-08-24 20:09:28 +0700

    Moved state to store object

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
 import * as serviceWorker from './serviceWorker';
-import state, {addMessage, addPost, subscribe, updateNewMessageText, updateNewPostText} from './redux/state';
+import store from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-let rerenderEntireTree = (state) => {
+let rerenderEntireTree = () => {
   ReactDOM.render(
     <React.StrictMode>
       <App
-        state={state}
-        addPost={addPost}
-        updateNewPostText={updateNewPostText}
-        addMessage={addMessage}
-        updateNewMessageText={updateNewMessageText}
+        state={store.getState()}
+        addPost={store.addPost.bind(store)}
+        addMessage={store.addMessage.bind(store)}
+        updateNewPostText={store.updateNewPostText.bind(store)}
+        updateNewMessageText={store.updateNewMessageText.bind(store)}
       />
     </React.StrictMode>,
     document.getElementById('root')
   );
 };
 
-rerenderEntireTree(state);
+rerenderEntireTree();
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,152 +1,148 @@
-let rerenderEntireTree = () => {
-  console.log("State has changed");
-};
-
-let state = {
-  profilePage: {
-    postsData: [
-      {
-        id: 1,
-        message: "first post",
-        likesCount: 3
-      },
-      {
-        id: 2,
-        message: "second post",
-        likesCount: 15
-      },
-      {
-        id: 3,
-        message: "third post message",
-        likesCount: 5
-      }
-    ],
-    newPostText: ""
+let store = {
+  _state: {
+    profilePage: {
+      postsData: [
+        {
+          id: 1,
+          message: "first post",
+          likesCount: 3
+        },
+        {
+          id: 2,
+          message: "second post",
+          likesCount: 15
+        },
+        {
+          id: 3,
+          message: "third post message",
+          likesCount: 5
+        }
+      ],
+      newPostText: ""
+    },
+    dialogsPage: {
+      messagesData: [
+        {
+          id: 1,
+          message: "Hi",
+          authorName: "Rashid",
+          authorAvatar: "https://www.svgrepo.com/show/65453/avatar.svg"
+        },
+        {
+          id: 2,
+          message: "How are you?",
+          authorName: "Rashid",
+          authorAvatar: "https://www.svgrepo.com/show/65453/avatar.svg"
+        },
+        {
+          id: 3,
+          message: "I'm fine, thanks",
+          authorName: "Name2",
+          authorAvatar: "https://www.svgrepo.com/show/137325/avatar.svg"
+        },
+        {
+          id: 4,
+          message: "See you later",
+          authorName: "Rashid",
+          authorAvatar: "https://www.svgrepo.com/show/65453/avatar.svg"
+        }
+      ],
+      dialogsData: [
+        {
+          id: 1,
+          name: "Name1",
+          avatar: "https://www.svgrepo.com/show/95333/avatar.svg"
+        },
+        {
+          id: 2,
+          name: "Name2",
+          avatar: "https://www.svgrepo.com/show/137325/avatar.svg"
+        },
+        {
+          id: 3,
+          name: "Rashid",
+          avatar: "https://www.svgrepo.com/show/65453/avatar.svg"
+        },
+        {
+          id: 4,
+          name: "Noname",
+          avatar: "https://www.svgrepo.com/show/9695/avatar.svg"
+        },
+        {
+          id: 5,
+          name: "Name 5",
+          avatar: "https://www.svgrepo.com/show/17344/avatar.svg"
+        },
+        {
+          id: 6,
+          name: "Name6",
+          avatar: "https://www.svgrepo.com/show/157054/avatar.svg"
+        }
+      ],
+      newMessageText: ""
+    },
+    sidebar: {
+      friends: [
+        {
+          id: 1,
+          name: "Rashid Ibragimov",
+          avatar: "https://www.svgrepo.com/show/65453/avatar.svg"
+        },
+        {
+          id: 2,
+          name: "Dmitry Petrov",
+          avatar: "https://www.svgrepo.com/show/63886/avatar.svg"
+        },
+        {
+          id: 3,
+          name: "Vasily Fedorov",
+          avatar: "https://www.svgrepo.com/show/83466/avatar.svg"
+        },
+        {
+          id: 4,
+          name: "Theodor Slonsky",
+          avatar: "https://www.svgrepo.com/show/29870/avatar.svg"
+        }
+      ]
+    }
   },
-  dialogsPage: {
-    messagesData: [
-      {
-        id: 1,
-        message: "Hi",
-        authorName: "Rashid",
-        authorAvatar: "https://www.svgrepo.com/show/65453/avatar.svg"
-      },
-      {
-        id: 2,
-        message: "How are you?",
-        authorName: "Rashid",
-        authorAvatar: "https://www.svgrepo.com/show/65453/avatar.svg"
-      },
-      {
-        id: 3,
-        message: "I'm fine, thanks",
-        authorName: "Name2",
-        authorAvatar: "https://www.svgrepo.com/show/137325/avatar.svg"
-      },
-      {
-        id: 4,
-        message: "See you later",
-        authorName: "Rashid",
-        authorAvatar: "https://www.svgrepo.com/show/65453/avatar.svg"
-      }
-    ],
-    dialogsData: [
-      {
-        id: 1,
-        name: "Name1",
-        avatar: "https://www.svgrepo.com/show/95333/avatar.svg"
-      },
-      {
-        id: 2,
-        name: "Name2",
-        avatar: "https://www.svgrepo.com/show/137325/avatar.svg"
-      },
-      {
-        id: 3,
-        name: "Rashid",
-        avatar: "https://www.svgrepo.com/show/65453/avatar.svg"
-      },
-      {
-        id: 4,
-        name: "Noname",
-        avatar: "https://www.svgrepo.com/show/9695/avatar.svg"
-      },
-      {
-        id: 5,
-        name: "Name 5",
-        avatar: "https://www.svgrepo.com/show/17344/avatar.svg"
-      },
-      {
-        id: 6,
-        name: "Name6",
-        avatar: "https://www.svgrepo.com/show/157054/avatar.svg"
-      }
-    ],
-    newMessageText: ""
+  _callSubscriber() {
+    console.log("no subscribers")
   },
-  sidebar: {
-    friends: [
-      {
-        id: 1,
-        name: "Rashid Ibragimov",
-        avatar: "https://www.svgrepo.com/show/65453/avatar.svg"
-      },
-      {
-        id: 2,
-        name: "Dmitry Petrov",
-        avatar: "https://www.svgrepo.com/show/63886/avatar.svg"
-      },
-      {
-        id: 3,
-        name: "Vasily Fedorov",
-        avatar: "https://www.svgrepo.com/show/83466/avatar.svg"
-      },
-      {
-        id: 4,
-        name: "Theodor Slonsky",
-        avatar: "https://www.svgrepo.com/show/29870/avatar.svg"
-      }
-    ]
+  getState() {
+    return this._state;
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
+  },
+  addPost() {
+    let newPost = {
+      id: 4,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0
+    };
+    this._state.profilePage.postsData.push(newPost);
+    this._state.profilePage.newPostText = "";
+    this._callSubscriber();
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber();
+  },
+  addMessage() {
+    let newMessage = {
+      id: 5,
+      message: this._state.dialogsPage.newMessageText,
+      authorName: "Some One",
+      authorAvatar: "https://www.svgrepo.com/show/65453/avatar.svg"
+    };
+    this._state.dialogsPage.messagesData.push(newMessage);
+    this._state.dialogsPage.newMessageText = "";
+    this._callSubscriber();
+  },
+  updateNewMessageText(newText) {
+    this._state.dialogsPage.newMessageText = newText;
+    this._callSubscriber();
   }
 };
-
-window.state = state;
-
-export const addPost = () => {
-  let newPost = {
-    id: 4,
-    message: state.profilePage.newPostText,
-    likesCount: 0
-  };
-  state.profilePage.postsData.push(newPost);
-  state.profilePage.newPostText = "";
-  rerenderEntireTree(state);
-};
-
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-};
-
-export const addMessage = () => {
-  let newMessage = {
-    id: 5,
-    message: state.dialogsPage.newMessageText,
-    authorName: "Some One",
-    authorAvatar: "https://www.svgrepo.com/show/65453/avatar.svg"
-  };
-  state.dialogsPage.messagesData.push(newMessage);
-  state.dialogsPage.newMessageText = "";
-  rerenderEntireTree(state);
-};
-
-export const updateNewMessageText = (newText) => {
-  state.dialogsPage.newMessageText = newText;
-  rerenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-};
-
-export default state;
+export default store;
